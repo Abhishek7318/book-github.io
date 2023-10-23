ec48cf3ad76b3d242c145dd1eddf41dfd8cd708e
Abhishek7318 <127573710+Abhishek7318@users.noreply.github.com>
2023-10-23 16:32:57 +0530

    update
    update

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-// Define an array to store posts
+let likes = 0;
+let commentss = 0;
+let likess = document.querySelector(".like");
+
+function like() {
+  likes++;
+  likess.className = 'click_red';
+  updateResult();
+}
+
+function likees() {
+    likes++;
+    likess.className = 'click_red';
+    updateResult();
+  }
+function comments() {
+  commentss++;
+  updateResult();
+}
+
+function updateResult() {
+  document.getElementById("result").innerText = "Likes: " + likes + " | comments: " + commentss;
+}
+
+let postText = document.querySelector("#postText");
+
+
+function addpost() {
+    const postDiv = document.createElement('div');
+    postDiv.classList.add('postd');
+    postDiv.innerHTML = postText.value;
+    document.getElementById('posts').appendChild(postDiv);
+        document.getElementById('postText').value = '';
+}
