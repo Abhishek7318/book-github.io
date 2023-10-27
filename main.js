40938ce2c7af4d818a292833af3ad63eb9b1e671
Abhishek7318 <127573710+Abhishek7318@users.noreply.github.com>
2023-10-27 21:54:37 +0530

    new update
    updates

--- main.js
+++ main.js
@@ -2,79 +2,79 @@ const allStories = [
     {
       id: 0,
       author: "Luna Belle",
-      imageUrl: "images/1.jpg",
+      imageUrl: "storyiesimg/1.jpg",
     },
   
     {
       id: 1,
       author: "Willow Grace",
-      imageUrl: "images/2.jpg",
+      imageUrl: "storyiesimg/2.jpg",
     },
   
     {
       id: 2,
       author: "Emma Smith",
-      imageUrl: "images/3.jpg",
+      imageUrl: "storyiesimg/3.jpg",
     },
   
     {
       id: 3,
       author: "Ruby Skye",
-      imageUrl: "images/4.jpg",
+      imageUrl: "storyiesimg/4.jpg",
     },
   
     {
       id: 4,
       author: "Live Blogger",
-      imageUrl: "images/5.jpg",
+      imageUrl: "storyiesimg/5.jpg",
     },
   
     {
       id: 5,
       author: "Hazel Jade",
-      imageUrl: "images/6.jpg",
+      imageUrl: "storyiesimg/6.jpg",
     },
   
     {
       id: 6,
       author: "Eden Faith",
-      imageUrl: "images/7.jpg",
+      imageUrl: "storyiesimg/7.jpg",
     },
   
     {
       id: 7,
       author: "Flora Maeve",
-      imageUrl: "images/8.jpg",
+      imageUrl: "storyiesimg/8.jpg",
     },
   
     {
       id: 8,
       author: "Nathaniel Hayes",
-      imageUrl: "images/9.jpg",
+      imageUrl: "storyiesimg/9.jpg",
     },
   
     {
       id: 9,
       author: "Everett Lee",
-      imageUrl: "images/10.jpg",
+      imageUrl: "storyiesimg/10.jpg",
     },
   
     {
       id: 9,
       author: "Owen",
-      imageUrl: "images/11.jpg",
+      imageUrl: "storyiesimg/11.jpg",
     },
   
     {
       id: 9,
       author: "Caleb Knox",
-      imageUrl: "images/12.jpg",
+      imageUrl: "storyiesimg/12.jpg",
     },
   
     {
       id: 9,
       author: "Felix",
-      imageUrl: "images/13.jpg",
+      imageUrl: "storyiesimg/13.jpg",
     },
   ];
   
